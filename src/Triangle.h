db551ad329a8a335fb53ce4fe3b94a996d27ed08
Zhongyin Hu <zhongyin.hu@berkeley.edu>
2013-12-04 22:13:14 -0800

    Update Triangle.h

--- src/Triangle.h
+++ src/Triangle.h
@@ -7,43 +7,47 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
 
+#include <windows.h>
+#include <GL/GL.h>
+#include <GL/glut.h>
+#include <GL/GLU.h>
+
 #include <Eigen/Dense>
+#include "Particle.h"
 
 using namespace Eigen;
 
 class Triangle
 {
 private:
-    Particle *mParticle1;
-    Particle *mParticle2;
-    Particle *mParticle3;
+    Particle *Par1;
+    Particle *Par2;
+    Particle *Par3;
 
-    float mTextureCoord[3][2];
 public:
-    Triangle(Particle *p1, Particle *p2, Particle *p3);
+	Triangle (Particle* p1, Particle* p2, Particle* p3)
+    {
+        Par1 = p1;
+        Par2 = p2;
+        Par3 = p3;
+    }
 
     Vector3f  getNormal();
 
     void Draw();
 
-    Particle* getParticle1() { return mParticle1; }
-    Particle* getParticle2() { return mParticle2; }
-    Particle* getParticle3() { return mParticle3; }
+    Particle* getParticle1() { return Par1; }
+    Particle* getParticle2() { return Par2; }
+    Particle* getParticle3() { return Par3; }
+
 };
 
 
-Triangle::Triangle(Particle *p1, Particle *p2, Particle *p3)
-    {
-        mParticle1 = p1;
-        mParticle2 = p2;
-        mParticle3 = p3;
-    }
-
     // return the normal of the current triangle
-    Vector3f Triangle::getNormal()
+ Vector3f Triangle::getNormal()
     {
-		Vector3f v1 = mParticle2->getPos()-mParticle1->getPos();
-		Vector3f v2 = mParticle3->getPos()-mParticle1->getPos();
+		Vector3f v1 = Par2->getPos()-Par1->getPos();
+		Vector3f v2 = Par3->getPos()-Par1->getPos();
 
         return v1.cross(v2).normalized();
     }
@@ -51,15 +55,15 @@ Triangle::Triangle(Particle *p1, Particle *p2, Particle *p3)
     // Draw the triangle
     void Triangle::Draw()
     {
-		glNormal3fv((float*)&(mParticle1->getNormal().normalized()));
-		glVertex3fv((float*)&(mParticle1->getPos()));
+		glNormal3fv((float*)&(Par1->getNormal().normalized()));
+		glVertex3fv((float*)&(Par1->getPos()));
 
-		glNormal3fv((float*)&(mParticle2->getNormal().normalized()));
-		glVertex3fv((float*)&(mParticle2->getPos()));
+		glNormal3fv((float*)&(Par2->getNormal().normalized()));
+		glVertex3fv((float*)&(Par2->getPos()));
 
-		glNormal3fv((float*)&(mParticle3->getNormal().normalized()));
-		glVertex3fv((float*)&(mParticle3->getPos()));
+		glNormal3fv((float*)&(Par3->getNormal().normalized()));
+		glVertex3fv((float*)&(Par3->getPos()));
     }
 
 
-#endif // TRIANGLE_H
+#endif 
