10a4e0b62b67212009a1a962ec3fb0db08002c3e
Zhongyin Hu <zhongyin.hu@berkeley.edu>
2013-12-09 23:23:01 -0800

    Update Cloth.h

--- src/Cloth.h
+++ src/Cloth.h
@@ -3,7 +3,6 @@
 
 	Kristen Curry, Zhongyin Hu, Alex Tom, Aaron Zhou
 ********************************************************************************/
-
 #ifndef CLOTH_H
 #define CLOTH_H
 
@@ -12,11 +11,9 @@
 #include <GL/glut.h>
 #include <GL/GLU.h>
 
-#include "Cloth.h"
 #include <vector>
 #include "Particle.h"
 #include "Spring.h"
-#include "Triangle.h"
 
 #include <Eigen/Dense>
 
@@ -24,187 +21,231 @@ using namespace Eigen;
 
 class Cloth
 {
-private:
-	 float mass;    // mass of cloth
-	 float width;  //width of cloth
-         float height;  //height of cloth
-	int HNum;  //number of horizontal particles
-	int VNum;  //number of vertical particles
-	float DampingCoeff;
-
-	std::vector<Particle> list_particles; // list of all particles of the cloth
-	std::vector<Spring> list_strings; //list of all constraints between particles of this cloth
-        std::vector<Triangle*> list_triangles;  //list of all the triangles 
-
-	Particle* getParticle(int x, int y) { return &list_particles[x*HNum + y]; }
-	void AddSpring(Particle *p1, Particle *p2) { list_strings.push_back(Spring(p1,p2)); }
-
-        void prepare();
-
 public:
 
-    Cloth(float widthvalue, float heightvalue, int horizontalParticles, int verticalParticles, float dampingCoeff)
-    {
-		width=widthvalue; 
-		height=heightvalue;
-		HNum=horizontalParticles;
-	        VNum=verticalParticles;
-		DampingCoeff=dampingCoeff;
+	int num_particles_x; // number of particles in x direction
+	int num_particles_y; // number of particles in y direction
+	
 
-                prepare();
+	std::vector<Particle> List_Particles; //used to store the info of particle
+	std::vector<Spring> List_Springs; 
+
+	Particle* getParticle(int x, int y) {return & List_Particles.at(x*num_particles_x + y);}
+	
+	void AddSpring(Particle *p1, Particle *p2) 
+	{   Spring newspring(p1,p2);
+		List_Springs.push_back(newspring);
+	}
+	
+	Cloth(float width, float height, int num_width, int num_height) 
+	{
+		num_particles_x=num_width; 
+		num_particles_y=num_height;
+
+		List_Particles.resize(num_particles_x*num_particles_y); 
+		
+		// creating a grid of particles from (0,0,0) to (width,-height,0)
+		for(int x=0; x<num_particles_x; x++)
+		{
+			for(int y=0; y<num_particles_y; y++)
+			{
+				float pos_x=width*(float)x/(float)num_particles_x;
+				float pos_y=-height*(float)y/(float)num_particles_y;
+				Vector3f pos = Vector3f(pos_x, pos_y, 0);
+				List_Particles.at(x*num_particles_x+y)=Particle(pos); 
+			}
+		}
+		
+		// making the upper left most two and upper right most two particles unmovable
+			getParticle(0,0)->makeUnmovable(); 
+			getParticle(1,0)->makeUnmovable(); 
+			getParticle(num_particles_x-1,0)->makeUnmovable();
+			getParticle(num_particles_x-2,0)->makeUnmovable();
+
+
+		for(int x=0; x<num_particles_x; x++)
+		{
+			for(int y=0; y<num_particles_y; y++)
+			{
+				// 1 structural springs
+				if (x<num_particles_x-1) 
+					AddSpring(getParticle(x,y),getParticle(x+1,y));
+				if (y<num_particles_y-1) 
+					AddSpring(getParticle(x,y),getParticle(x,y+1));
+
+				// 2 shear springs
+				if (x<num_particles_x-1 && y<num_particles_y-1) 
+					AddSpring(getParticle(x,y),getParticle(x+1,y+1));
+				if (x<num_particles_x-1 && y<num_particles_y-1) 
+					AddSpring(getParticle(x+1,y),getParticle(x,y+1));
+	
+		        // 3 bend springs
+				if (x<num_particles_x-2) 
+					AddSpring(getParticle(x,y),getParticle(x+2,y));
+				if (y<num_particles_y-2) 
+					AddSpring(getParticle(x,y),getParticle(x,y+2));
+				if (x<num_particles_x-2 && y<num_particles_y-2) 
+					AddSpring(getParticle(x,y),getParticle(x+2,y+2));
+				if (x<num_particles_x-2 && y<num_particles_y-2) 
+					AddSpring(getParticle(x+2,y),getParticle(x,y+2));			
+			}
+		}
+
 	}
 
-    float getMass() { return mass; }
-    void setMass(float m)
-    {
-                mass = m;
-		float mass = m/(HNum*VNum);
-		for(int i = 0; i < list_particles.size(); i++) list_particles[i].setMass(mass);
-    }
+	Vector3f TriangleNormal(Particle *p1,Particle *p2,Particle *p3)
+	{
+		Vector3f pos1 = p1->getPos();
+		Vector3f pos2 = p2->getPos();
+		Vector3f pos3 = p3->getPos();
 
-    float getWidth() { return width; }
-    void setWidth(float w) { width = w; prepare();}
-    
-    float getHeight() { return height; }
-    void setHeight(float h) { height = h; prepare();}
+		Vector3f v1 = pos2-pos1;
+		Vector3f v2 = pos3-pos1;
 
-    float getDampingCoeff() { return DampingCoeff; }
-    void setDampingCoeff(float coeff) { DampingCoeff = coeff; }
+		Vector3f trianglenormal=v1.cross(v2);
+		return trianglenormal;
+	}
 
-    int getNumOfHorizontalParticles() { return HNum; }
-    void setNumOfHorizontalParticles(float hPart) { HNum = hPart; prepare();}
-    
-    int getNumOfVerticalParticles() { return VNum; }
-    void setNumOfVerticalParticles(float vPart) { VNum = vPart; prepare();}
+	
+	void drawTriangle(Particle *p1, Particle *p2, Particle *p3, const Vector3f color)
+	{
+		glColor3f(color[0], color[1],color[2]);
 
-	void Draw();
+		glNormal3f(p1->getNormal().normalized()[0], p1->getNormal().normalized()[1],p1->getNormal().normalized()[2]);
+		glVertex3f(p1->getPos()[0], p1->getPos()[1], p1->getPos()[2]);
 
-	void Update();
+		glNormal3f(p2->getNormal().normalized()[0], p2->getNormal().normalized()[1],p2->getNormal().normalized()[2]);
+		glVertex3f(p2->getPos()[0], p2->getPos()[1], p2->getPos()[2]);
 
-	void AddForce(Vector3f direction); // adds forces to the particles 
+		glNormal3f(p3->getNormal().normalized()[0], p3->getNormal().normalized()[1],p3->getNormal().normalized()[2]);
+		glVertex3f(p3->getPos()[0], p3->getPos()[1], p3->getPos()[2]);
 
-	void Intersect(float r, Vector3f p);//Sphere Collision detection using the position and radius of the sphere 
-    
+	}
+
+	void drawCloth()
+	{
+		std::vector<Particle>::iterator it;
+		for(it = List_Particles.begin(); it!= List_Particles.end(); it++)
+		{
+			(*it).resetNormal();
+		}
+
+	
+		for(int x = 0; x<num_particles_x-1; x++)
+		{
+			for(int y=0; y<num_particles_y-1; y++)
+			{
+				Vector3f normal = TriangleNormal(getParticle(x+1,y),getParticle(x,y),getParticle(x,y+1));
+				getParticle(x+1,y)->addToNormal(normal);
+				getParticle(x,y)->addToNormal(normal);
+				getParticle(x,y+1)->addToNormal(normal);
+
+				normal = TriangleNormal(getParticle(x+1,y+1),getParticle(x+1,y),getParticle(x,y+1));
+				getParticle(x+1,y+1)->addToNormal(normal);
+				getParticle(x+1,y)->addToNormal(normal);
+				getParticle(x,y+1)->addToNormal(normal);
+			}
+		}
+
+		glBegin(GL_TRIANGLES);
+		for(int x = 0; x<num_particles_x-1; x++)
+		{
+			for(int y=0; y<num_particles_y-1; y++)
+			{
+				Vector3f color = Vector3f(0.5f, 0.5f, 0.5f);
+
+				drawTriangle(getParticle(x+1,y),getParticle(x,y),getParticle(x,y+1),color);
+				drawTriangle(getParticle(x+1,y+1),getParticle(x+1,y),getParticle(x,y+1),color);
+			}
+		}
+		glEnd();
+	}
+
+	#define NUMBER_ITERATIONS 10
+
+	void Time()
+	{
+		std::vector<Spring>::iterator it;
+		for(int i=0; i<NUMBER_ITERATIONS; i++) // iterate over all Springs several times
+		{
+			for(it = List_Springs.begin(); it != List_Springs.end(); it++ )
+			{
+				(*it).CheckSpring(); 
+			}
+		}
+
+		std::vector<Particle>::iterator iter;
+		for(iter= List_Particles.begin(); iter != List_Particles.end(); iter++)
+		{
+			(*iter).Time(); // calculate the position of each particle at the next time step.
+		}
+	}
+
+	/* used to add gravity/other external force to all particles*/
+	void AddForce(const Vector3f direction)
+	{
+		std::vector<Particle>::iterator it;
+		for(it = List_Particles.begin(); it!= List_Particles.end(); it++)
+		{
+			(*it).AddForce(direction); // add the forces to each particle
+		}
+
+	}
+
+	/* used to add wind forces to all particles*/
+	//REFERENCE：http://www.cs.huji.ac.il/~irush/cloth/
+	/*The wind adds force to the particle in the direction of its normal 
+	with size equal to the dot product between the normal and the wind's direction, multiplied by the power parameter
+   */
+
+	/* start by looking at the cloth as a collection of triangles of three particles;
+	  1. Solve the problem of adding wind one triangle at a time;
+	  2. Adding forces to the three particles of the triangle; 
+	  3. The wind is coming in a certain direction, but forces should only be added in the direction of the normal of the triangle;
+	  4. The amount of force affecting the triangle should be proportional to the angle (dot product) between the triangle and the wind. 
+	*/
+
+	void AddWindForce(Particle *p1,Particle *p2,Particle *p3, const Vector3f direction)
+	{
+		Vector3f normal = TriangleNormal(p1,p2,p3);
+		Vector3f d = normal.normalized();
+		Vector3f force = normal*(d.dot(direction));
+		p1->AddForce(force);
+		p2->AddForce(force);
+		p3->AddForce(force);
+	}
+
+	void AddWind(const Vector3f direction)
+	{
+		for(int x = 0; x<num_particles_x-1; x++)
+		{
+			for(int y=0; y<num_particles_y-1; y++)
+			{
+				AddWindForce(getParticle(x+1,y),getParticle(x,y),getParticle(x,y+1),direction);
+				AddWindForce(getParticle(x+1,y+1),getParticle(x+1,y),getParticle(x,y+1),direction);
+			}
+		}
+	}
+
+	// used to detect the collision of the cloth with the ball.
+	
+	void Intersect(const Vector3f center,const float radius )
+	{
+		std::vector<Particle>::iterator it;
+		for(it = List_Particles.begin(); it!= List_Particles.end(); it++)
+		{
+			Vector3f v = (*it).getPos()-center;
+			float l = v.norm();
+			if ( v.norm() < radius) // if the particle is inside the ball
+			{
+				float e=1;
+				(*it).movePos(v.normalized()*((radius+e)-l)); 
+				// move the particle to the surface of the ball
+				// add e for a better image
+			}
+		}
+	}
+
+
 };
-
-void Cloth::prepare()
-{
-    list_particles.clear();
-	list_particles.resize(HNum*VNum); 
-
-   
-	for(int x = 0; x < HNum; x++)
-    {
-        for(int y = 0; y < VNum; y++)
-        {
-            // Compute the initial cloth particle positions 
-			Vector3f pos = Vector3f(width * (x/(float)HNum), height * (y/(float)VNum), 0);
-			list_particles[x*HNum+y]= Particle(pos); 
-        }
-    }
-
-    // Prepare the triangles that are being drawn
-    for(int x = 0; x < HNum-1; x++)
-    {
-        for(int y = 0; y < VNum-1; y++)
-        {
-            // Construct triangles between the particles and add them to the cloth
-            Triangle *tr1 = new Triangle(getParticle(x+1,y),getParticle(x,y),getParticle(x,y+1));
-            Triangle *tr2 = new Triangle(getParticle(x+1,y+1),getParticle(x+1,y),getParticle(x,y+1));
-
-            getParticle(x+1,y)->AddTriangle(tr1);
-            getParticle(x,y)->AddTriangle(tr1);
-            getParticle(x,y+1)->AddTriangle(tr1);
-
-            getParticle(x+1,y+1)->AddTriangle(tr2);
-            getParticle(x+1,y)->AddTriangle(tr2);
-            getParticle(x,y+1)->AddTriangle(tr2);
-
-            list_triangles.push_back(tr1);
-            list_triangles.push_back(tr2);
-        }
-    }
-
-       //construct springs bewteen different particles
-	   
-    for(int x = 0; x < HNum; x++)
-    {
-        for(int y = 0; y < VNum; y++)
-        {
-			// 1 structural springs
-            if (x < HNum-1) AddSpring(getParticle(x,y),getParticle(x+1,y));
-            if (y < VNum-1) AddSpring(getParticle(x,y),getParticle(x,y+1));
-			
-			// 2 shear springs
-            if (x < HNum-1 && y < VNum-1) AddSpring(getParticle(x,y),getParticle(x+1,y+1));
-            if (x < HNum-1 && y < VNum-1) AddSpring(getParticle(x+1,y),getParticle(x,y+1));
-   
-			//3 bending springs
-            if (x < HNum-2) AddSpring(getParticle(x,y),getParticle(x+2,y));
-            if (y < VNum-2) AddSpring(getParticle(x,y),getParticle(x,y+2));
-            if (x < HNum-2 && y<VNum-2) AddSpring(getParticle(x,y),getParticle(x+2,y+2));
-            if (x  <HNum-2 && y<VNum-2) AddSpring(getParticle(x+2,y),getParticle(x,y+2));			
-	}
-    }
-}
-
-
-void Cloth::Intersect(float r, Vector3f p)
-{
-    
-    for(int i = 0; i < list_particles.size(); i++)
-    {
-        Particle &particle = list_particles[i];
-
-        Vector3f v = particle.getPosition()-p;
-        float l = v.norm();
-        if ( v.norm() < r) // if the particle is inside the sphere
-        {
-            particle.movePos(v.normalized()*r);
-        }
-    }
-}
-
-
-void Cloth::AddForce(const Vector3f direction)
-{
-    std::vector<Particle>::iterator particle;
-    for(particle = list_particles.begin(); particle != list_particles.end(); particle++)
-    {
-        (*particle).AddForce(direction); // add the forces to each particle
-    }
-
-}
-
-void Cloth::Update()
-{
-		for(int j = 0; j < list_strings.size(); j++)
-        {
-            // satisfy constraint
-			list_strings[j].Check();
-        }
-
-    std::vector<Particle>::iterator it;
-	for(it= list_particles.begin(); it != list_particles.end(); it++)
-    {
-        (*it).Update(DampingCoeff, 0.25f);
-    }
-}
-
-
-void Cloth::Draw()
-{
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glBegin(GL_TRIANGLES);
-
-    // Draw each triangle of the cloth
-    for(int i = 0; i < list_triangles.size(); i++)
-        list_triangles[i]->Draw();
-
-    glEnd();
-}
-
-
-
-
-#endif // CLOTH_H
+#endif 
